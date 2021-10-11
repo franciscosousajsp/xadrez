7926333965300e7d8c892c49cdcff679ccf14379
= <=>
2021-10-11 17:04:45 -0300

    criando a classe posicao

---
+++ src/principal/Principal.java
@@ -0,0 +1,15 @@
+package principal;
+
+import tabuleiro.Posicao;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Posicao pos = new Posicao(5, 3);
+		
+		System.out.println(pos);
+	}
+
+}
